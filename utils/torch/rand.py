ca617eeef8dd14ecfd4977e1d4b44cbe171b76cd
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2021-03-15 17:03:18 +0000

    workable version for codes. plz check the entropy lower bound of codes for comparison of loss (in bits scale)

--- utils/torch/rand.py
+++ utils/torch/rand.py
@@ -32,9 +32,13 @@ def logistic_logp(mu, scale, x):
 # function to calculate the log-probability of x under a discretized Logistic(mu, scale) distribution
 # heavily based on discretized_mix_logistic_loss() in https://github.com/openai/pixel-cnn
 def discretized_logistic_logp(mu, scale, x):
+    # scale_min = -7.0
+    # scale = torch.maximum(scale, scale_min)
     # [0,255] -> [-1.1] (this means bin sizes of 2./255.)
     # x_rescaled = (x - 127.5) / 127.5
     # [0,511] -> [-1.1] (this means bin sizes of 2./511.)
+    # can regard mu as predicted output.
+    # 	x as Target.
     x_rescaled = (x - 255.5) / 255.5
     invscale = 1. / scale
     # mu 8x8
@@ -71,6 +75,48 @@ def discretized_logistic_logp(mu, scale, x):
 
     logp = logps.flatten(1)
     return logp
+
+# can be train for codes, loss is around 5.0, but atually compression doesnt help
+# def discretized_logistic_logp(mu, scale, x):
+#     # [0,255] -> [-1.1] (this means bin sizes of 2./255.)
+#     x_rescaled = (x - 127.5) / 127.5
+#     # [0,511] -> [-1.1] (this means bin sizes of 2./511.)
+#     # x_rescaled = (x - 255.5) / 255.5
+#     invscale = 1. / scale
+#     # mu 8x8
+#     x_centered = x_rescaled - mu
+#
+#     plus_in = invscale * (x_centered + 1. / 255.)
+#     # plus_in = invscale * (x_centered + 1. / 511.)
+#     cdf_plus = torch.sigmoid(plus_in)
+#     min_in = invscale * (x_centered - 1. / 255.)
+#     # min_in = invscale * (x_centered - 1. / 511.)
+#     cdf_min = torch.sigmoid(min_in)
+#
+#     # log-probability for edge case of 0
+#     log_cdf_plus = plus_in - modules.softplus(plus_in)
+#
+#     # log-probability for edge case of 255
+#     log_one_minus_cdf_min = - modules.softplus(min_in)
+#
+#     # other cases
+#     cdf_delta = cdf_plus - cdf_min
+#
+#     mid_in = invscale * x_centered
+#
+#     # log-probability in the center of the bin, to be used in extreme cases
+#     log_pdf_mid = mid_in - torch.log(scale) - 2. * modules.softplus(mid_in)
+#
+#     # now select the right output: left edge case, right edge case, normal case, extremely low-probability case
+#     cond1 = torch.where(cdf_delta > 1e-5, torch.log(torch.clamp(cdf_delta, min=1e-12, max=None)),
+#                         log_pdf_mid - np.log(127.5))
+#     # cond1 = torch.where(cdf_delta > 1e-5, torch.log(torch.clamp(cdf_delta, min=1e-12, max=None)),
+#     #                     log_pdf_mid - np.log(255.5))
+#     cond2 = torch.where(x_rescaled > .999, log_one_minus_cdf_min, cond1)
+#     logps = torch.where(x_rescaled < -.999, log_cdf_plus, cond2)
+#
+#     logp = logps.flatten(1)
+#     return logp
 
 # function to calculate the CDF of the Logistic(mu, scale) distribution evaluated under x
 def logistic_cdf(x, mu, scale):
@@ -165,3 +211,31 @@ class ImageBins:
         centres = (centres - 255.5) / 255.5
         centres = centres[None,].expand(self.shape + [-1])
         return centres
+
+# ImageBins for image will casue error for codes ' CUDA error: device-side assert triggered'
+#
+
+# class ImageBins:
+#     def __init__(self, type, device, shape):
+#         # datatype used
+#         self.type = type
+#
+#         # device used (CPU/GPU)
+#         self.device = device
+#         self.shape = [shape]
+#
+#     def endpoints(self):
+#         endpoints = torch.arange(1, 256, dtype=self.type, device=self.device)
+#         endpoints = ((endpoints - 127.5) / 127.5) - 1./255.
+#         # endpoints = torch.arange(1, 512, dtype=self.type, device=self.device)
+#         # endpoints = ((endpoints - 255.5) / 255.5) - 1. / 511.
+#         endpoints = endpoints[None,].expand(self.shape + [-1])
+#         return endpoints
+#
+#     def centres(self):
+#         centres = torch.arange(0, 256, dtype=self.type, device=self.device)
+#         centres = (centres - 127.5) / 127.5
+#         # centres = torch.arange(0, 512, dtype=self.type, device=self.device)
+#         # centres = (centres - 255.5) / 255.5
+#         centres = centres[None,].expand(self.shape + [-1])
+#         return centres
